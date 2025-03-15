fdbe331ab1bfa0adbd8de72eaf0c056474c73d33
homow <homowsh897@gmail.com>
2025-03-15 18:06:22 +0330

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -5,35 +5,31 @@ const customMenu = document.querySelector('.wrapper__custom-context-menu');
 const openMenu = document.querySelector('.open__nav-menu i');
 const navMenu = document.querySelector('.nav-menu');
 
-// disable links
-document.querySelectorAll("body a").forEach(link => {
-    link.addEventListener("click", event => {
-        event.preventDefault();
-    })
-})
-
 // ----------------- context menu ----------------
 // open context menu
-const contextMenuHandler = event => {
+const contextMenuHandler = (menu, event) => {
     event.preventDefault();
-    customMenu.style.display = 'flex';
-    const spaceBelow = window.innerHeight + window.scrollY - (event.pageY + customMenu.offsetHeight)
-    const spaceRight = window.innerWidth - (event.pageX + customMenu.offsetWidth)
 
-    customMenu.style.top = spaceBelow < 0 ? `${event.pageY - customMenu.offsetHeight}px` : `${event.pageY}px`
-    customMenu.style.left = spaceRight < 0 ? `${event.pageX - customMenu.offsetWidth}px` : `${event.pageX}px`
+    if (!menu.contains(event.target)) {
+        menu.style.display = 'flex';
+        const spaceBelow = window.innerHeight + window.scrollY - (event.pageY + menu.offsetHeight)
+        const spaceRight = window.innerWidth - (event.pageX + menu.offsetWidth)
+
+        menu.style.top = spaceBelow < 0 ? `${event.pageY - menu.offsetHeight}px` : `${event.pageY}px`
+        menu.style.left = spaceRight < 0 ? `${event.pageX - menu.offsetWidth}px` : `${event.pageX}px`
+    }
 }
 // close context menu with click
-const closeContextMenu = event => {
-    if (customMenu.style.display !== 'none' && !event.target.matches(".wrapper__custom-context-menu *")) {
-        customMenu.style.display = 'none';
+const closeContextMenu = (menu, event) => {
+    if (menu.style.display !== 'none' && !event.target.matches(".wrapper__custom-context-menu *")) {
+        menu.style.display = 'none';
     }
 }
 
 // close context menu withe escape
-const closeContextMenuKey = event => {
-    if (customMenu.style.display !== 'none' && event.key === 'Escape') {
-        customMenu.style.display = 'none';
+const closeContextMenuKey = (menu, event) => {
+    if (menu.style.display !== 'none' && event.key === 'Escape') {
+        menu.style.display = 'none';
     }
 }
 
@@ -52,8 +48,11 @@ const setTheme = (wrapper, root) => {
     wrapper.className = theme === "dark" ? "fas fa-sun" : "fas fa-moon";
 }
 
-// ----------------- open nav menu -----------------
+// ----------------- nav menu -----------------
+// open nav menu
 const openMenuHandler = (menu, event) => {
+    event.stopPropagation()
+
     if (event.target.classList.contains("fa-bars")) {
         menu.style.left = "0"
         event.target.className = "fa fa-close"
@@ -63,11 +62,30 @@ const openMenuHandler = (menu, event) => {
     }
 }
 
+//close nav menu
+const closeNavMenu = (menu, btn, cstMenu, event) => {
+    const elems = [menu, cstMenu]
+    const checkClick = elems.every(elem => {return !elem.contains(event.target)}) // Check that the click is outside these elements.
+
+    if (checkClick) {
+        menu.style.left = "-160px"
+        btn.className = "fa fa-bars"
+    }
+}
+
 // events
-document.body.addEventListener("dblclick", contextMenuHandler) // context menu
-document.body.addEventListener("contextmenu", contextMenuHandler); // context menu
-document.addEventListener("keydown", closeContextMenuKey); // closes menu with escape
-document.body.addEventListener("click", closeContextMenu) // closes context menu
+document.body.addEventListener("dblclick", contextMenuHandler.bind(null, customMenu)) // context menu
+document.body.addEventListener("contextmenu", contextMenuHandler.bind(null, customMenu)); // context menu
+document.body.addEventListener("keydown", closeContextMenuKey.bind(null, customMenu)); // closes menu with escape
+document.body.addEventListener("click", closeContextMenu.bind(null, customMenu)) // closes context menu
 changeTheme.addEventListener("click", changeThemeHandler.bind(null, rootElem)); // changes theme
 setTheme(changeTheme, rootElem); // sets theme
-openMenu.addEventListener("click", openMenuHandler.bind(null, navMenu));
+openMenu.addEventListener("click", openMenuHandler.bind(null, navMenu)); // open nav menu
+document.body.addEventListener("click", closeNavMenu.bind(null, navMenu, openMenu, customMenu)) // close nav menu
+
+// disable links
+document.querySelectorAll("body a").forEach(link => {
+    link.addEventListener("click", event => {
+        event.preventDefault();
+    })
+})
